87c434b40a1c5f5a50e4bee9b2994d079791b408
Inaridiy <inainainariinainari@gmail.com>
2022-08-17 15:41:08 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-base-200 flex h-screen w-full items-center justify-center px-4">
+      <div className="card card-body bg-base-100 shadow-lg">
+        <h1 className="text-xl font-bold">Welcome Web2 Template.</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
