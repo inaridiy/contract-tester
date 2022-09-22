56ccf976d6e3a62a412045d46c2519666bf461bf
Inaridiy <inainainariinainari@gmail.com>
2022-09-22 14:07:46 +0900

    PWA

--- src/components/ContractsTab.tsx
+++ src/components/ContractsTab.tsx
@@ -9,26 +9,29 @@ export const ContractsTab = () => {
   return (
     <div className="tabs tabs-boxed mx-auto">
       {contractDataList &&
-        Object.keys(contractDataList).map((key) => (
-          <button
-            key={key}
-            className={clsx(
-              "tab tab-lg items-center",
-              contractTag === key && "tab-active"
-            )}
-            onClick={() => setContractTag(key)}
-          >
-            {key}
-            {contractTag === key && (
-              <button
-                className="btn btn-square btn-ghost btn-sm"
-                onClick={() => removeContract(key)}
-              >
-                <XMarkIcon className="h-8 w-8" />
-              </button>
-            )}
-          </button>
-        ))}
+        Object.keys(contractDataList).map((key) =>
+          contractTag !== key ? (
+            <button
+              key={key}
+              className={clsx("tab tab-lg items-center")}
+              onClick={() => setContractTag(key)}
+            >
+              {key}
+            </button>
+          ) : (
+            <div key={key} className="tab tab-lg tab-active items-center">
+              {key}
+              {contractTag === key && (
+                <button
+                  className="btn btn-square btn-ghost btn-sm"
+                  onClick={() => removeContract(key)}
+                >
+                  <XMarkIcon className="h-8 w-8" />
+                </button>
+              )}
+            </div>
+          )
+        )}
     </div>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,37 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Contract Tester",
+        short_name: "CTester",
+        description: "A Simple Contract Test Tool",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": `${__dirname}/src/`,
     },
   },
-})
+});
