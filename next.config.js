b976e8cecff382d1118c1b034558b7e1b0736c02
inaridiy <inainainariinainari@gmail.com>
2022-11-19 14:54:48 +0900

    feat: PWA

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
+});
--- src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module "*.map" {
-  const content: string;
-
-  export default content;
-}
