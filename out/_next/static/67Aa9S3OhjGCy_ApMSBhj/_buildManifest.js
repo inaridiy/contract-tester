0773e43a215f5b00b40abdeeb9d86f2878dddcb6
inaridiy <inainainariinainari@gmail.com>
2022-11-19 16:00:12 +0900

    export

---
+++ out/_next/static/67Aa9S3OhjGCy_ApMSBhj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-fb3de0d6e5d691c8.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
