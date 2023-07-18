8bcfbb4cd3236e234db8065c8a14407ca204b858
Inaridiy <inainainariinainari@gmail.com>
2023-07-18 23:34:27 +0900

    reset window

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,9 @@
 import { TabsDemo } from "@/components/component";
-import { WindowApplication } from "@/components/window/window-application";
-import { WindowContainer } from "@/components/window/window-container";
 
 export default function Home() {
   return (
     <>
-      <WindowContainer className="h-[100svh] w-full">
-        <WindowApplication windowKey="demo-tab" name="Tabs Demo">
-          <TabsDemo />
-        </WindowApplication>
-        <WindowApplication windowKey="demo-tab2" name="Tabs Demo2">
-          <TabsDemo />
-        </WindowApplication>
-
-        <WindowApplication windowKey="demo-tab3" name="Tabs Demo3">
-          <TabsDemo />
-        </WindowApplication>
-
-        <WindowApplication windowKey="demo-tab4" name="Tabs Demo4">
-          <TabsDemo />
-        </WindowApplication>
-      </WindowContainer>
+      <TabsDemo />
     </>
   );
 }
